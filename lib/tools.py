fee1d55643b632c00ce51876b7e4fa13dec326b2
Junicchi <24779257+KebabLord@users.noreply.github.com>
2020-12-07 20:01:44 +0300

    modulise takeyoki and add wait command

---
+++ lib/tools.py
@@ -0,0 +1,160 @@
+""" This module includes some static functions """
+import secrets
+import string
+import re
+from inspect import cleandoc
+from random import randint
+import sys
+import argparse
+import requests
+from requests.exceptions import ConnectionError as rConnectionError, Timeout, ConnectTimeout
+from bs4 import BeautifulSoup as bs4
+
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class MailTools:
+    @staticmethod
+    def read_mail(mail):
+        """
+        Parse mail html.
+        First gets the solid text, then prints the buttons with links and titles.
+        """
+        print(f"{Colors.BOLD}Title:{Colors.ENDC}",mail.title)
+        print(f"{Colors.BOLD}From:{Colors.ENDC}",mail.sender)
+        soup = bs4(mail.text,"html.parser")
+        raw = soup.get_text()
+        text = '\n'.join([x for x in raw.splitlines() if x.strip()])
+        # Remove first line because it's duplicate of title.
+        text = text[text.index("\n")+1:]
+        print(text+"\n")
+        # Get buttons and links
+        linx = soup.findAll('a', attrs={'href': re.compile("^http[s,]://")})
+        for i in linx:
+            prefix = (f"{Colors.BOLD}{Colors.OKGREEN}({i.getText().strip()})↴{Colors.ENDC}\n","")
+            prefix = prefix[ i.getText() == '' ]
+            print(prefix+PrtTools.decrypt_url(i.get("href")))
+
+    @staticmethod
+    def find_mailbox(val,box=None):
+        """
+        Find mailbox from given search pattern, if digit, use it as index
+        If string, search it on mail addresses. Also if box is not passed
+        search it on cached index instead.
+        Ex:
+          find_mailbox("1",box) -> returns mails[1]
+          find_mailbox("usagi",box) -> returns "usagimo321@example.co"
+        """
+        try:
+            if box:
+                if val.isdigit():
+                    return box.mails[int(val)-1]
+                return [i for i in box.mails if val in i.address][0]
+            #Use Cache instead if box is not specified.
+            with open(".mails","r") as file:
+                mails = file.read().strip().split("\n")
+            if val.isdigit():
+                return mails[int(val)-1]
+            return [i for i in mails if val in i][0]
+        except IndexError:
+            print("Mail couldn't found.")
+            sys.exit(1)
+
+class PrtTools:
+    @staticmethod
+    def gen_details(password=None,nick=None):
+        """ Generate safe passwords and random usernames """
+        if password:
+            chars = string.digits + string.ascii_letters + "#$%&@?=.;:"
+            return ''.join(secrets.choice(chars) for i in range(15))
+        if nick:
+            nick =''.join(secrets.choice(string.ascii_lowercase) for i in range(6))
+            return nick+str(randint(0,999))
+        return None
+
+    @staticmethod
+    def escape_ansi(var):
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        return ansi_escape.sub('', var)
+
+    @staticmethod
+    def clear_line():
+        print('\033[2K\033[1G',end="\r")
+
+    @staticmethod
+    def decrypt_url(url):
+        """ Decrypt redirect url by parsing metadata of gateaway. """
+        print("Trying to decrypt gateaway urls..",end="\r")
+        try:
+            req = requests.get(url)
+            PrtTools.clear_line()
+            return re.findall('href = "(.*)"',req.text)[0]
+        except (Timeout, ConnectTimeout,rConnectionError):
+            PrtTools.clear_line()
+            return url
+
+class Params:
+    """ Register parameters and subcommands """
+    parser = None
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        subparsers = self.parser.add_subparsers(dest="command",required=True)
+
+        comm_read = subparsers.add_parser("read",help="Read mails")
+        comm_read.add_argument("--address","-a", required=False,
+            help="Select address from it's index or name")
+
+        comm_del = subparsers.add_parser("del",help="Delete a mailbox")
+        comm_del.add_argument("--address","-a",  required=True,
+            help="Select address from it's index or name")
+
+        subparsers.add_parser("list",help="List mail addresses")
+        subparsers.add_parser("create",help="Create new random address")
+
+        comm_gen = subparsers.add_parser("gen",
+            help="Generate random usernames/passwords and save them")
+        comm_gen.add_argument(
+            "--address","-a", required=True,help="Select address from it's index or name")
+        comm_gen.add_argument(
+            "--save","-s",    required=False, help="Save account details with specified tag")
+        comm_gen.add_argument(
+            "--password","-p",required=False, help="Generate password",action="store_true")
+        comm_gen.add_argument(
+            "--uname","-u",   required=False, help="Generate username",action="store_true")
+        comm_gen.add_argument(
+            "--wait","-w",   required=False, help="Start listening mails",action="store_true")
+
+        comm_w8 = subparsers.add_parser("wait",help="Wait for new mails")
+        comm_w8.add_argument("--address","-a",  required=True,
+            help="Select address from it's index or name")
+
+        custom_help = cleandoc(
+            """Takoyaki 1.0, an interactive burner mail creator and controller.
+            Usage: kuku [SUBCOMMAND] [OPTION]...
+
+            subcommands:
+              create [-h]               Create new random mail address
+              read   [-ha]              Read mails from specified address
+              wait   [-ha]              Wait for new mails on specified address
+              del    [-ha]              Delete specified mail address
+              list   [-h]               List current mail addresses
+              gen    [-hwpusa]          Generate registration details
+
+            parameters:
+              -h, --help                Show extended information about the subcommand
+              -a, --address <ADDRESS>   Specify the mail address
+              -p, --password            Generate password
+              -u, --uname               Generate username
+              -w, --Wait                Start listening new mails after generate
+              -s, --save <NAME>         Save generated info to .accs file
+            """)+"\n"
+        self.parser.format_help = custom_help.__str__
--- takoyaki.py
+++ takoyaki.py
@@ -3,106 +3,145 @@
 from os.path import exists
 import sys
 from PyInquirer import prompt
-from requests.exceptions import Timeout, ConnectTimeout
-import tools
-from sute import Sute
+from requests.exceptions import ConnectionError as rConnectionError, Timeout, ConnectTimeout
 
-args = tools.parser.parse_args()
-Colors = tools.Colors
+from lib.tools import Params,Colors,MailTools,PrtTools
+from lib.sute import Sute
 
-def connect():
-    try:
-        print("Connecting..",end="\r")
-        if exists(".ses"):
-            with open(".ses") as ses_file:
-                ses_id = ses_file.read()
+class Takoyaki:
+    def __init__(self):
+        self.box = None
+
+    def connect(self):
+        """ Creates the mail box object which includes addresses,mails and ids """
+        try:
+            print("Connecting..",end="\r")
+            if exists(".ses"):
+                with open(".ses") as ses_file:
+                    ses_id = ses_file.read()
+            else:
+                ses_id = None
+            sute = Sute(ses_id=ses_id)
+            PrtTools.clear_line()
+            with open(".mails","w") as cache:
+                cache.writelines([mail.address+"\n" for mail in sute.mails])
+            #return sute
+            self.box=sute
+        except (Timeout, ConnectTimeout,rConnectionError):
+            print("Connection problem.")
+            sys.exit(1)
+
+    def list(self):
+        self.box.refresh_address_list()
+        for i,mail in enumerate(self.box.mails):
+            print("{}  {}".format(i+1,mail.address))
+
+    def create(self):
+        new = self.box.create_new_random_address()
+        print("created:",new.address)
+        with open(".mails","w") as file:
+            file.writelines([mail.address+"\n" for mail in self.box.mails])
+
+    def gen(self):
+        """
+        Generates a random password and a username
+        optionally saves them to .accs file with given tag
+        """
+        if exists(".mails"):
+            selected_mail=MailTools.find_mailbox(args.address)
         else:
-            ses_id = None
-        sute = Sute(ses_id=ses_id)
-        tools.clearLine()
-        with open(".mails","w") as cache:
-            cache.writelines([mail.address+"\n" for mail in sute.mails])
-        return sute
-    except (ConnectionError, Timeout, ConnectTimeout):
-        print("Connection problem.")
-        sys.exit(1)
+            self.connect()
+            selected_mail=MailTools.find_mailbox(args.address,self.box).address
 
-if args.command in ["del","read","create","list","wait"]:
-    box=connect()
+        result=f"{Colors.BOLD}Mail:{Colors.ENDC} {selected_mail} \n"
+        if args.uname:
+            result+=f"{Colors.BOLD}Nick:{Colors.ENDC} {PrtTools.gen_details(nick=True)} \n"
+        if args.password:
+            result+=f"{Colors.BOLD}Pass:{Colors.ENDC} {PrtTools.gen_details(password=True)} \n"
+        if args.save:
+            result=f"{Colors.BOLD}=== {args.save.title()} Account ==={Colors.ENDC}\n"+result
+            with open(".accs","a") as file:
+                file.write(PrtTools.escape_ansi(result)+"\n")
+        print(result)
+        if args.wait:
+            self.connect()
+            self.wait()
 
-if args.command == "create":
-    new = box.create_new_random_address()
-    print("created:",new.address)
-    with open(".mails","w") as f:
-        f.writelines([mail.address+"\n" for mail in box.mails])
+    def delete(self):
+        mailbox = MailTools.find_mailbox(args.address,self.box)
+        status = mailbox.delete_mailbox()
+        if status==200:
+            print("Success.")
 
-elif args.command == "list":
-    box.refresh_address_list()
-    for x,y in enumerate(box.mails):
-        print("{}  {}".format(x+1,y.address))
-
-elif args.command == "gen":
-    if exists(".mails"):
-        selected_mail=tools.find_mailbox(args.address)
-    else:
-        box = connect()
-        selected_mail=tools.find_mailbox(args.address,box).address
-
-    result=f"{Colors.BOLD}Mail:{Colors.ENDC} {selected_mail} \n"
-    if args.uname:
-        result+=f"{Colors.BOLD}Nick:{Colors.ENDC} {tools.gen_nick()} \n"
-    if args.password:
-        result+=f"{Colors.BOLD}Pass:{Colors.ENDC} {tools.gen_password()} \n"
-    if args.save:
-        result=f"{Colors.BOLD}=== {args.save.title()} Account ==={Colors.ENDC}\n"+result
-        with open(".accs","a") as f:
-            f.write(result+"\n")
-    print(result)
-
-elif args.command == "del":
-    mailbox = tools.find_mailbox(args.address,box)
-    status = mailbox.delete_mailbox()
-    if status==200:
-        print("Success.")
-
-elif args.command == "read":
-    if args.address:
-        selected_mailbox = tools.find_mailbox(args.address,box)
-        pass
-    else:
-        selected_mailbox = prompt([{
-            'type': 'list',
-            'name': 'mailbox',
-            'message': 'Select mailbox',
-            'choices': [{a:b for a,b in (("name",i.address),("value",i))} for i in box.mails]
+    def read(self):
+        """ Read mails, prompts interactive cli if target mail box is not specified """
+        if args.address:
+            selected_mailbox = MailTools.find_mailbox(args.address,self.box)
+        else:
+            selected_mailbox = prompt([{
+                'type': 'list',
+                'name': 'mailbox',
+                'message': 'Select mailbox',
+                'choices': [{a:b for a,b in (("name",i.address),("value",i))}
+                            for i in self.box.mails]
             }])["mailbox"]
 
-    mails = selected_mailbox.get_mail_list()
-    mail_list=[{a:b for a,b in (("name",i.title),("value",i))} for i in mails]
-    if mail_list:
-        selected_mail = prompt([{
-            'type': 'list',
-            'name': 'mail',
-            'message': 'Select mail',
-            'choices':  mail_list
-            }])["mail"]
-        tools.read_mail(selected_mail)
-    else:
-        print("No mails.")
-elif args.command == "wait":
-    selected_mailbox = tools.find_mailbox(args.address,box)
-    print(f"Waiting new mails for {selected_mailbox.address}")
-    mails=selected_mailbox.get_mail_list()
-    print(f"Last mail was: {mails[0]}\nPress Ctrl+C to Abort.",end="\r")
-    count = cur_count = len(mails)
-    while count == cur_count:
-        try:
-            mails=selected_mailbox.get_mail_list()
-            cur_count = len(mails)
-        except KeyboardInterrupt:
-            print('Aborted.')
-            sys.exit()
-        except (ConnectionError, Timeout, ConnectTimeout):
-            pass
-    tools.clearLine()
-    tools.read_mail(mails[0])
+        mails = selected_mailbox.get_mail_list()
+        mail_list=[{a:b for a,b in (("name",i.title),("value",i))} for i in mails]
+        if mail_list:
+            selected_mail = prompt([{
+                'type': 'list',
+                'name': 'mail',
+                'message': 'Select mail',
+                'choices':  mail_list
+                }])["mail"]
+            MailTools.read_mail(selected_mail)
+        else:
+            print("No mails.")
+
+    def wait(self):
+        """ Listen for new messages on specified mailbox """
+        selected_mailbox = MailTools.find_mailbox(args.address,self.box)
+        print(f"Waiting new mails for {selected_mailbox.address}")
+        mails=selected_mailbox.get_mail_list()
+        print(f"Last mail was: {mails[0]}\nPress Ctrl+C to Abort.",end="\r")
+        count = cur_count = len(mails)
+        while count == cur_count:
+            try:
+                mails=selected_mailbox.get_mail_list()
+                cur_count = len(mails)
+            except (KeyboardInterrupt,TypeError):
+                PrtTools.clear_line()
+                print('Aborted.')
+                sys.exit()
+            except (ConnectionError, Timeout, ConnectTimeout):
+                pass
+        PrtTools.clear_line()
+        MailTools.read_mail(mails[0])
+
+
+if __name__ == "__main__":
+    params = Params()
+    args = params.parser.parse_args()
+    yaki = Takoyaki()
+
+    if args.command in ["del","read","create","list","wait"]:
+        yaki.connect()
+
+    if args.command == "create":
+        yaki.create()
+
+    elif args.command == "list":
+        yaki.list()
+
+    elif args.command == "gen":
+        yaki.gen()
+
+    elif args.command == "del":
+        yaki.delete()
+
+    elif args.command == "read":
+        yaki.read()
+
+    elif args.command == "wait":
+        yaki.wait()
